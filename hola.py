0afe23f2f849f943217bb9637943ce1e2ae4bc7d
Fernando Mauro <fermaurolf@gmail.com>
2025-09-25 09:28:52 -0500

    Mejore mi programa

--- hola.py
+++ hola.py
@@ -2,3 +2,5 @@
 print("¿Por qué los programadores confunden Halloween con Navidad?")
 input("Presiona Enter para ver la respuesta...")
 print("Porque OCT 31 es igual a DEC 25!")
+
+input("Presiona Enter para salir...")
